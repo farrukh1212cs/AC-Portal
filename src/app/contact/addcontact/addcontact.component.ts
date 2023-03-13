318a981e42af038c5ae034be53959023fb03f1eb
Farrukh Rehman <farrukh1212cs@outlook.com>
2023-03-13 09:48:46 +0500

    check in working on meterial UI

--- src/app/contact/addcontact/addcontact.component.ts
+++ src/app/contact/addcontact/addcontact.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ContactService } from '../contact.service';
 import { MatCardModule } from '@angular/material/card';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CreateContactDto } from '../CreateContactDto';
 
 
@@ -11,60 +11,91 @@ import { CreateContactDto } from '../CreateContactDto';
   templateUrl: './addcontact.component.html',
   styleUrls: ['./addcontact.component.css']
 })
-export class AddcontactComponent {
-  officeLocationDropdownValues :any =  [];
+export class AddcontactComponent  implements OnInit {
+  contactForm!: FormGroup;
+  contactDto?: CreateContactDto;
 
-  constructor(private dialogRef: MatDialogRef<AddcontactComponent>,private contactService: ContactService,private dialog: MatDialog) {
+  constructor(private formBuilder: FormBuilder) { }
 
-
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      company: [''],
+      addressLine1: [''],
+      addressLine2: [''],
+      city: [''],
+      zipCode: [''],
+      email: ['', Validators.email],
+      website: [''],
+      faxNo: [''],
+      displayName: [''],
+      startDate: [''],
+      endDate: [''],
+      discription: [''],
+      file: [''],
+      sourceId: [''],
+      stateId: [''],
+      salesRepId: [''],
+      subContractorId: [''],
+      teamMembers: [''],
+      officeLocationId: [''],
+      workFlowId: [''],
+      statusId: [''],
+      relatedContacts: [''],
+      tags: [''],
+      note: this.formBuilder.group({
+        text: ['']
+      }),
+      phoneNumbers: this.formBuilder.array([
+        this.formBuilder.group({
+          type: [''],
+          number: ['']
+        })
+      ]),
+      customFields: this.formBuilder.array([
+        this.formBuilder.group({
+          name: [''],
+          value: ['']
+        })
+      ])
+    });
   }
 
-  closeAddContactsModal() {
-    this.dialogRef.close();
-  }
-  ngOnInit() {
-    this.getOfficeLocation();
-   }
-  saveData() {
-    // Your code to save user's data to the database goes here
+  onSubmit(): void {
+    console.log(this.contactForm);
+    if (this.contactForm.invalid) {
+      console.log(this.contactForm);
+    }
 
-    // Close the dialog
-    this.dialogRef.close();
+    this.contactDto = this.contactForm.value;
+    console.log('Contact DTO:', this.contactDto);
+    // Call your service to save the contact data
   }
-  //------------------
-  getOfficeLocation(){
-    this.contactService.allOfficeLocations().subscribe(
-      res => {
-        this.officeLocationDropdownValues = res.payload;
-      },
-      err => {
-        alert(err);
-      },
-      () => {
-       
-      }
-    );
+
+  addPhoneNumber(): void {
+    // const phoneNumbers = this.contactForm.get('phoneNumbers') as FormGroup;
+    // phoneNumbers.push(this.formBuilder.group({
+    //   type: [''],
+    //   number: ['']
+    // }));
   }
- 
-  createNewContact() {
-    const newContact: CreateContactDto = {
-      firstName: 'John',
-      lastName: 'Doe',
-      company: 'Acme Inc.'
-      
-      // ... other properties of CreateContactDto
-    };
-  
-    this.contactService.createContact(newContact).subscribe(
-      (response) => {
-        console.log(response);
-        // handle success response
-      },
-      (error) => {
-        console.log(error);
-        // handle error response
-      }
-    );
+
+  removePhoneNumber(index: number): void {
+    // const phoneNumbers = this.contactForm.get('phoneNumbers') as FormGroup;
+    // phoneNumbers.removeAt(index);
   }
-  
+
+  addCustomField(): void {
+    // const customFields = this.contactForm.get('customFields') as FormGroup;
+    // customFields.push(this.formBuilder.group({
+    //   name: [''],
+    //   value: ['']
+    // }));
+  }
+
+  removeCustomField(index: number): void {
+    // const customFields = this.contactForm.get('customFields') as FormGroup;
+    // customFields.removeAt(index);
+  }
 }
