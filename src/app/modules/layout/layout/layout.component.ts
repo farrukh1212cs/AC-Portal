919cc2a5b04d6cf4a7efeec5a02997ceb1c5eee4
DPLIT\zamad.t <zamad.t@dplit.com>
2023-06-06 19:20:43 +0500

    UI Imprvements

--- src/app/modules/layout/layout/layout.component.ts
+++ src/app/modules/layout/layout/layout.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild , HostListener} from '@angular/core';
+
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
+  public getScreenWidth: any;
+  @ViewChild('layout')
+  layout!: ElementRef;
 
+  ngAfterViewInit (){
+    this.getScreenWidth = window.innerWidth
+    if(this.getScreenWidth > 992){
+      this.layout.nativeElement.classList.add("sidebar-slim")
+    }
+    // if(this.getScreenWidth > 600 && this.getScreenWidth < 992){
+    //   this.layout.nativeElement.classList.add("tablet","sidebar-slim")
+    // }
+    // if(this.getScreenWidth < 600){
+    //   this.layout.nativeElement.classList.add("mobile")
+    // }
+  }
+
+  toggleSidebar(elem:HTMLElement) {
+    this.getScreenWidth = window.innerWidth
+    if(this.getScreenWidth > 992){
+      elem.classList.toggle("sidebar-slim")
+      elem.classList.toggle("sidebar-expanded")
+      // if(elem.classList.contains("sidebar-slim")){
+      //   elem.classList.toggle("sidebar-slim")
+      //   elem.classList.toggle("sidebar-expanded")
+      // }
+    }
+    // if(this.getScreenWidth > 600 && this.getScreenWidth < 992 ){
+    //   if(elem.classList.contains("tablet")){
+    //     elem.classList.toggle("sidebar-slim")
+    //   }
+    // }
+    // if(this.getScreenWidth < 600 ){
+    //   if(elem.classList.contains("mobile")){
+    //     elem.classList.toggle("sidebar-mobile-open")
+    //   }
+    // }
+  }
 }
--- src/app/modules/layout/sidebar/sidebar.component.ts
+++ src/app/modules/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-
+  @Output() toogleSidebaremit = new EventEmitter<string>();
+  
+  toggleSidebar(){
+    this.toogleSidebaremit.emit();
+  }
 }
